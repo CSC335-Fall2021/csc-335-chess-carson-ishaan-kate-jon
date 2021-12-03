6d0885956537619d5c019e6dde2654c086cb6a86
Carson Chapman <carson@azchapmana.com>
2021-12-03 13:24:14 -0700

    Fixed Formatting

--- src/controller/ChessController.java
+++ src/controller/ChessController.java
@@ -4,27 +4,26 @@ import model.ChessModel;
 import model.Move;
 
 public class ChessController {
-	
+
 	private ChessModel CurModel;
-	
-	public ChessController() {					// New Game
-		this(null);		
+
+	public ChessController() { // New Game
+		this(null);
 	}
-	
-	public ChessController(String fenString) {	// Existing Game
+
+	public ChessController(String fenString) { // Existing Game
 		CurModel = new ChessModel(fenString);
 	}
-	
-	public ChessModel getModel() {				// Retrieve chess Model
+
+	public ChessModel getModel() { // Retrieve chess Model
 		return this.CurModel;
 	}
-	
-	public String getFenString() {				// Retrieve encoded board
+
+	public String getFenString() { // Retrieve encoded board
 		return CurModel.getFenString();
 	}
-	
-	public void makePlayerMove(Move oldMove, 
-							   Move newMove) {	// Perform a player Move
+
+	public void makePlayerMove(Move oldMove, Move newMove) { // Perform a player Move
 		CurModel.makeMove(oldMove, newMove);
 	}
 
--- src/model/ChessModel.java
+++ src/model/ChessModel.java
@@ -4,116 +4,109 @@ import javafx.beans.InvalidationListener;
 import javafx.beans.Observable;
 
 public class ChessModel {
-	
-		private boolean thisTurn = false;
-		Piece[][] chessBoard;
-		private static final String starterString = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR";
-		
-		public ChessModel() {
-			this(starterString);
+
+	private boolean thisTurn = false;
+	Piece[][] chessBoard;
+	private static final String starterString = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR";
+
+	public ChessModel() {
+		this(starterString);
+	}
+
+	public ChessModel(String fenString) {
+		chessBoard = new Piece[8][8];
+		if (fenString == null) {
+			createChessBoard(starterString);
+		} else {
+			createChessBoard(fenString);
 		}
-		
-		public ChessModel(String fenString) {
-			chessBoard = new Piece[8][8];
-			if (fenString == null) {
-				createChessBoard(starterString);
-			} else {
-				createChessBoard(fenString);
-			}
+	}
+
+	public void setMyTurn(boolean mine) {
+		thisTurn = mine;
+	}
+
+	public boolean isMyTurn() {
+		return thisTurn;
+	}
+
+	public Move[] getPossibleMoves(int x, int y) {
+		Piece curPiece = chessBoard[x][y];
+		if (curPiece.getType() == 'p' || curPiece.getType() == 'P') {
+			// TODO implement pawn logic
+		} else if (curPiece.getType() == 'r' || curPiece.getType() == 'R') {
+			// TODO implement rook logic
+		} else if (curPiece.getType() == 'n' || curPiece.getType() == 'N') {
+			// TODO implement knight logic
+		} else if (curPiece.getType() == 'b' || curPiece.getType() == 'B') {
+			// TODO implement bishop logic
+		} else if (curPiece.getType() == 'q' || curPiece.getType() == 'Q') {
+			// TODO implement queen logic
+		} else if (curPiece.getType() == 'k' || curPiece.getType() == 'K') {
+			// TODO implement king logic
 		}
-		
-		public void setMyTurn(boolean mine) {
-			thisTurn = mine;
-		}
-		
-		public boolean isMyTurn() {
-			return thisTurn;
-		}
-		
-		public Move[] getPossibleMoves(int x, int y) {
-			Piece curPiece = chessBoard[x][y];
-			if (curPiece.getType() == 'p' || curPiece.getType() == 'P') {
-				//TODO implement pawn logic
-			}
-			else if (curPiece.getType() == 'r' || curPiece.getType() == 'R') {
-				//TODO implement rook logic
-			}
-			else if (curPiece.getType() == 'n' || curPiece.getType() == 'N') {
-				//TODO implement knight logic
-			}
-			else if (curPiece.getType() == 'b' || curPiece.getType() == 'B') {
-				//TODO implement bishop logic
-			}
-			else if (curPiece.getType() == 'q' || curPiece.getType() == 'Q') {
-				//TODO implement queen logic
-			}
-			else if (curPiece.getType() == 'k' || curPiece.getType() == 'K') {
-				//TODO implement king logic
-			}
-			return null;
-		}
-		
-		public void makeMove(Move oldMove, Move newMove) {
-			Piece emptyReplacement = new Piece(' ', oldMove.getX(), oldMove.getY());
-			chessBoard[newMove.getX()][newMove.getY()] = chessBoard[oldMove.getX()][oldMove.getY()];
-			chessBoard[oldMove.getX()][oldMove.getY()] = emptyReplacement;
-		}
-		
-		public void createChessBoard(String fenString) {
-			int file = 0;
-			int rank = 0;
-			for (int i=0; i < fenString.length(); i++) {
-				char piece = fenString.charAt(i);
-				if (piece == '/') {
-					file = 0;
-					rank++;
-				}
-				else if (Character.isAlphabetic(piece)) {
-					Piece newPiece = new Piece(piece, file, rank);
+		return null;
+	}
+
+	public void makeMove(Move oldMove, Move newMove) {
+		Piece emptyReplacement = new Piece(' ', oldMove.getX(), oldMove.getY());
+		chessBoard[newMove.getX()][newMove.getY()] = chessBoard[oldMove.getX()][oldMove.getY()];
+		chessBoard[oldMove.getX()][oldMove.getY()] = emptyReplacement;
+	}
+
+	public void createChessBoard(String fenString) {
+		int file = 0;
+		int rank = 0;
+		for (int i = 0; i < fenString.length(); i++) {
+			char piece = fenString.charAt(i);
+			if (piece == '/') {
+				file = 0;
+				rank++;
+			} else if (Character.isAlphabetic(piece)) {
+				Piece newPiece = new Piece(piece, file, rank);
+				chessBoard[rank][file] = newPiece;
+				file++;
+			} else if (!Character.isAlphabetic(piece)) {
+				for (int j = 0; j < Integer.parseInt("" + piece); j++) {
+					Piece newPiece = new Piece(' ', file, rank);
 					chessBoard[rank][file] = newPiece;
-					file++; 
-				}
-				else if (!Character.isAlphabetic(piece)) { 
-					for (int j=0; j < Integer.parseInt("" + piece); j++) {
-						Piece newPiece = new Piece(' ', file, rank);
-						chessBoard[rank][file] = newPiece;
-						file++;
-					}
+					file++;
 				}
 			}
 		}
-		
-		public String getFenString() {
-			return getFenString(this.chessBoard);
-		}
-		
-		public String getFenString(Piece[][] curboard) {
-			String result = "";
-			int count = 0; 
-			for (int row = 0; row < 8; row++) {
-				count = 0;
-				
-				for (int col = 0; col < 8; col++) {
-					// If slot is space
-					if (curboard[row][col].getFenRep() == ' ') {
-						
-						count++;
-					} else {
-						// Else is a piece 
-						if (count != 0) {
-							result += count;
-							count = 0;
-						}
-						result += curboard[row][col].getFenRep();
-					}
-					if (count != 0 && col == 7) {
+	}
+
+	public String getFenString() {
+		return getFenString(this.chessBoard);
+	}
+
+	public String getFenString(Piece[][] curboard) {
+		String result = "";
+		int count = 0;
+		for (int row = 0; row < 8; row++) {
+			count = 0;
+
+			for (int col = 0; col < 8; col++) {
+				// If slot is space
+				if (curboard[row][col].getFenRep() == ' ') {
+
+					count++;
+				} else {
+					// Else is a piece
+					if (count != 0) {
 						result += count;
+						count = 0;
 					}
+					result += curboard[row][col].getFenRep();
 				}
-				if (row < 7) {
-					result += '/';
+				if (count != 0 && col == 7) {
+					result += count;
 				}
 			}
-			return result;
+			if (row < 7) {
+				result += '/';
+			}
 		}
+		return result;
+	}
 }
